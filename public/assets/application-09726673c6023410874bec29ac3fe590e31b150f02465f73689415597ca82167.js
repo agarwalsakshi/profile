6bc65b3cfff3a9ff19329c15716b924dc5b37f69
Sakshi Agarwal <agg.sakshi1994@gmail.com>
2015-09-24 16:25:46 +0530

    removed all scss files

---
+++ public/assets/application-09726673c6023410874bec29ac3fe590e31b150f02465f73689415597ca82167.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+$("div#form1").append(
+$("<form/>", {
+action: '#',
+method: '#'
+}).append(
+$("<input/>", 
+{
+type: 'text',
+id: 'vname',
+name: 'name',
+placeholder: 'Your Name'
+}),
+
+
+
+$("<input/>", {
+type: 'text',
+id: 'vemail',
+name: 'email',
+placeholder: 'Your Email'
+}), 
+
+
+$("<textarea/>", {
+rows: '5px',
+cols: '27px',
+type: 'text',
+id: 'vmsg',
+name: 'msg',
+placeholder: 'Message'
+}), 
+
+
+$("<br/>"), $("<input/>", {
+type: 'submit',
+id: 'submit',
+value: 'Submit'
+})))
+});
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+/// require turbolinks
+//= require_tree .
